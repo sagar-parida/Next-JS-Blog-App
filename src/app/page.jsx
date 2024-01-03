56d41c921640b8b21029bc233c964e003bf025c9
sagar-parida <paridag321@gmail.com>
2024-01-03 11:32:40 +0530

    Layout and Global CSS Updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,12 @@
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi assumenda ratione, placeat, saepe dolorum et sunt quisquam, eaque sequi mollitia eum ipsa aperiam deleniti dolore. Sit unde accusamus repellat nostrum?
+    </h1>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, placeat, repellat voluptatem consectetur quae ad minus, officia neque nulla error expedita laboriosam nostrum porro. Odio atque quos omnis maiores est.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. At beatae veniam, ab minima quidem facere commodi dolorem ad magnam eligendi aliquid perferendis accusamus nihil nostrum laudantium rem eum nulla asperiores?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cumque nam ullam reprehenderit. Atque ut, cum vitae consectetur repellat fugit sequi sapiente cumque. Officia quam similique, laborum dolor harum doloremque.
+  </div>;
 }
